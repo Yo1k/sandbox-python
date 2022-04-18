778b28671ae724dc6b3aa33546e9141467a32627
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-04-18 18:14:47 +0300

    Address review concerns

--- yo1k/sandbox/exercises/CSC_algorithms/sorting.py
+++ yo1k/sandbox/exercises/CSC_algorithms/sorting.py
@@ -1,131 +1,158 @@
 from collections.abc import MutableSequence
-from typing import Any, Optional
+from typing import Any
 
 
-def swap(array: MutableSequence[Any], frst_idx: int, snd_idx: int) -> MutableSequence[Any]:
-    """Swaps to elements in a list."""
-    array[frst_idx], array[snd_idx] = array[snd_idx], array[frst_idx]
-    return array
+def swap(seq: MutableSequence[Any], frst_idx: int, snd_idx: int) -> MutableSequence[Any]:
+    """Swaps two elements in the `seq`."""
+    if seq[frst_idx] == seq[snd_idx]:
+        return seq
+    seq[frst_idx], seq[snd_idx] = seq[snd_idx], seq[frst_idx]
+    return seq
 
 
-def selection_sort(array: MutableSequence[Any]) -> MutableSequence[Any]:
+def selection_sort(seq: MutableSequence[Any]) -> MutableSequence[Any]:
     """Sorting by selection.
 
     Algorithm in-place, complexity: T(n) = O(n**2).
     """
-    size: int = len(array)
+    size: int = len(seq)
     for i in range(size):
         min_idx: int = -1
         for j in range(i, size):
-            if min_idx == -1 or array[j] <= array[min_idx]:
+            if min_idx == -1 or seq[j] < seq[min_idx]:
                 min_idx = j
-        swap(array, min_idx, i)
-    return array
+        swap(seq, min_idx, i)
+    return seq
 
 
-def insertion_sort(array: MutableSequence[Any]) -> MutableSequence[Any]:
+def insertion_sort(seq: MutableSequence[Any]) -> MutableSequence[Any]:
     """Sorting by inserts.
 
     Algorithm in-place, complexity: T(n) = O(n**2).
     """
-    for i in range(len(array)):
+    for i in range(len(seq)):
         j: int = i
-        while j > 0 and array[j - 1] > array[j]:
-            swap(array, j-1, j)
+        while j > 0 and seq[j - 1] > seq[j]:
+            swap(seq, j-1, j)
             j -= 1
-    return array
+    return seq
 
 
-def merge_sorted_arrays(
-        array_left: MutableSequence[Any],
-        array_right: MutableSequence[Any],
-        merged_array: Optional[MutableSequence[Any]] = None) -> MutableSequence[Any]:
-    """Merges two sorted arrays in ascending order."""
-    merged_array = [] if merged_array is None else merged_array
-    l_idx: int = 0
-    r_idx: int = 0
-    l_end: int = len(array_left)
-    r_end: int = len(array_right)
-
-    while l_idx < l_end and r_idx < r_end:
-        if array_left[l_idx] <= array_right[r_idx]:
-            merged_array.append(array_left[l_idx])
-            l_idx += 1
-        else:
-            merged_array.append(array_right[r_idx])
-            r_idx += 1
-    while l_idx < l_end:
-        merged_array.append(array_left[l_idx])
+def replace_subseq(
+        seq: MutableSequence[Any],
+        begin_subseq: int,
+        end_subseq: int,
+        tmp_seq: MutableSequence[Any]) -> None:
+    """Replaces a subsequence in the `seq` by elements from the beginning of the `tmp_seq`."""
+    l_idx = begin_subseq
+    for i in range(end_subseq - begin_subseq + 1):
+        assert l_idx <= end_subseq
+        seq[l_idx] = tmp_seq[i]
         l_idx += 1
-    while r_idx < r_end:
-        merged_array.append(array_right[r_idx])
-        r_idx += 1
-    return merged_array
 
 
-def merge_sort(array: MutableSequence[Any]) -> MutableSequence[Any]:
+def merge_sorted_seqs(
+        seq: MutableSequence[Any],
+        tmp_seq: MutableSequence[Any],
+        l_begin_seq: int,
+        l_end_seq: int,
+        r_begin_seq: int,
+        r_end_seq: int) -> None:
+    """Merges two sorted seqs in ascending order."""
+    l_seq_idx: int = l_begin_seq
+    r_seq_idx: int = r_begin_seq
+    l_seq_len: int = l_end_seq - l_begin_seq + 1
+    r_seq_len: int = r_end_seq - r_begin_seq + 1
+    tmp_seq_idx = 0
+
+    while l_seq_idx - l_begin_seq < l_seq_len and r_seq_idx - r_begin_seq < r_seq_len:
+        if seq[l_seq_idx] <= seq[r_seq_idx]:
+            tmp_seq[tmp_seq_idx] = seq[l_seq_idx]
+            tmp_seq_idx += 1
+            l_seq_idx += 1
+        else:
+            tmp_seq[tmp_seq_idx] = seq[r_seq_idx]
+            tmp_seq_idx += 1
+            r_seq_idx += 1
+    while l_seq_idx - l_begin_seq < l_seq_len:
+        tmp_seq[tmp_seq_idx] = seq[l_seq_idx]
+        tmp_seq_idx += 1
+        l_seq_idx += 1
+    while r_seq_idx - r_begin_seq < r_seq_len:
+        tmp_seq[tmp_seq_idx] = seq[r_seq_idx]
+        tmp_seq_idx += 1
+        r_seq_idx += 1
+
+    replace_subseq(seq, l_begin_seq, r_end_seq, tmp_seq)
+
+
+def merge_sort(seq: MutableSequence[Any]) -> MutableSequence[Any]:
     """Mergesort.
 
-    This algorithm has complexity O(n*log(n)) on condition
-    when we do not take into account overheads of creating new arrays in the process of sorting,
-    only comparison operation."""
-    size: int = len(array)
-    l_size: int = size // 2
-    r_size: int = size // 2
-    if size % 2 != 0:
-        l_size += 1
+    This algorithm has complexity O(n*log(n)).
+    """
+    size = len(seq)
+    tmp_seq = [None for _ in range(size)]
+    if size == 0:
+        return seq
 
-    if r_size == 0:
-        return array
-    l_array = array[: l_size]
-    r_array = array[l_size:]
-    assert len(l_array) == l_size and len(r_array) == r_size
-    l_array = merge_sort(l_array)
-    r_array = merge_sort(r_array)
-    sorted_array = merge_sorted_arrays(l_array, r_array)
-    return sorted_array
+    def _merge_sort(_seq: MutableSequence[Any], begin: int, end: int) -> MutableSequence[Any]:
+        delimetr_idx = (end - begin) // 2
+        l_seq_begin = begin
+        l_seq_end = l_seq_begin + delimetr_idx
+        r_seq_begin = l_seq_end + 1
+        r_seq_end = end
+
+        if r_seq_end - l_seq_begin == 0:
+            return _seq
+        # sort left subseq
+        _merge_sort(_seq, begin=l_seq_begin, end=l_seq_end)
+        # sort right subseq
+        _merge_sort(_seq, begin=r_seq_begin, end=r_seq_end)
+        # merge left and right subseqs
+        merge_sorted_seqs(seq, tmp_seq, l_seq_begin, l_seq_end, r_seq_begin, r_seq_end)
+        return _seq
+    return _merge_sort(seq, 0, size - 1)
 
 
-def partition(array: MutableSequence[Any], l_idx: int, r_idx: int) -> int:
-    """Changes `array` to: elements <= pivot, then a pivot element, elements > pivot.
+def partition(seq: MutableSequence[Any], l_idx: int, r_idx: int) -> int:
+    """Rearranges the `seq` around the pivot.
 
-    Without loss of generality the pivot is taken as the `l_idx`-th element of the `array`.
+    Rearranges the sequence such that all elements that are less than or equal to the pivot have
+    smaller indices than the pivot,
+    and all elements that are greater than the pivot have higher indices than the pivot.
+    Without loss of generality the pivot is taken as the `l_idx`-th element of the `seq`.
     """
     pivot_idx = l_idx
     i = l_idx + 1
     j = r_idx
     while i != j:
-        if array[i] > array[pivot_idx]:
-            swap(array, i, j)
+        if seq[i] > seq[pivot_idx]:
+            swap(seq, i, j)
             j -= 1
         else:
-            swap(array, i, pivot_idx)
+            swap(seq, i, pivot_idx)
             pivot_idx = i
             i += 1
-    if array[pivot_idx] >= array[i]:
-        swap(array, pivot_idx, i)
+    if seq[pivot_idx] >= seq[i]:
+        swap(seq, pivot_idx, i)
         pivot_idx = i
     return pivot_idx
 
 
-def quick_sort(
-            array: MutableSequence[Any],
-            begin: int = 0, end: Optional[int] = None) -> MutableSequence[Any]:
+def quick_sort(seq: MutableSequence[Any]) -> MutableSequence[Any]:
     """Quicksort or Hoare's sort.
 
-    Algorithm in-place, complexity: O(n*log(n)) for totally unsorted an array.
+    Algorithm in-place, complexity: O(n*log(n)) for a totally unsorted seq.
     """
-    if end is None:
-        end = len(array) - 1
-
-    def _quicksort(
-            _array: MutableSequence[Any],
+    def _quick_sort(
+            _seq: MutableSequence[Any],
             _begin: int,
             _end: int) -> MutableSequence[Any]:
         if _begin >= _end:
-            return _array
-        pivot = partition(_array, _begin, _end)
-        _quicksort(_array, _begin, pivot-1)
-        _quicksort(_array, pivot+1, _end)
-        return array
-    return _quicksort(array, begin, end)
+            return _seq
+        pivot = partition(_seq, _begin, _end)
+        _quick_sort(_seq, _begin, pivot - 1)
+        _quick_sort(_seq, pivot + 1, _end)
+        return _seq
+    return _quick_sort(seq, 0, len(seq) - 1)
--- yo1k/sandbox/exercises/CSC_algorithms/test_sorting.py
+++ yo1k/sandbox/exercises/CSC_algorithms/test_sorting.py
@@ -1,4 +1,4 @@
-import itertools
+import copy
 from collections.abc import Sequence, MutableSequence
 from typing import Any
 
@@ -7,8 +7,8 @@ import pytest
 import yo1k.sandbox.exercises.CSC_algorithms.sorting as my_sort
 
 
-@pytest.fixture
-def arrays_set() -> Sequence[MutableSequence[Any]]:
+@pytest.fixture(name="lists_set")
+def fixture_lists_set() -> Sequence[MutableSequence[Any]]:
     return (
             [1, 2, 5, 7, 3, 10, 4, -1, 5],
             [1, 2, 3, 2, 7, 8, 4, 5, 2],
@@ -19,35 +19,37 @@ def arrays_set() -> Sequence[MutableSequence[Any]]:
             [3, 3, 1, 2, 1, 2, 1],
             [-3, 1],
             [-1],
-            [])
+            [],
+            [[[1], [2]], [[1]], [[3]], [[1], [2]]],
+            ["a", "c", "x", "b"])
 
 
 def test_swap() -> None:
     assert my_sort.swap([1, 2, 5, 7, 3, 10, 4, -1, 5], 2, 4) == [1, 2, 3, 7, 5, 10, 4, -1, 5]
 
 
-def test_selection_sort(arrays_set: Sequence[MutableSequence[Any]]) -> None:
-    for array in arrays_set:
-        array1, array2 = itertools.tee(array)
-        assert my_sort.selection_sort(list(array1)) == sorted(list(array2))
+def test_selection_sort(lists_set: Sequence[MutableSequence[Any]]) -> None:
+    for lst in lists_set:
+        lst1, lst2 = lst, copy.copy(lst)
+        assert my_sort.selection_sort(lst1) == sorted(lst2)
 
 
-def test_insertion_sort(arrays_set: Sequence[MutableSequence[Any]]) -> None:
-    for array in arrays_set:
-        array1, array2 = itertools.tee(array)
-        assert my_sort.insertion_sort(list(array1)) == sorted(list(array2))
+def test_insertion_sort(lists_set: Sequence[MutableSequence[Any]]) -> None:
+    for lst in lists_set:
+        lst1, lst2 = lst, copy.copy(lst)
+        assert my_sort.insertion_sort(lst1) == sorted(lst2)
 
 
-def test_merge_sort(arrays_set: Sequence[MutableSequence[Any]]) -> None:
-    for array in arrays_set:
-        array1, array2 = itertools.tee(array)
-        assert my_sort.merge_sort(list(array1)) == sorted(list(array2))
+def test_merge_sort(lists_set: Sequence[MutableSequence[Any]]) -> None:
+    for lst in lists_set:
+        lst1, lst2 = lst, copy.copy(lst)
+        assert my_sort.merge_sort(lst1) == sorted(lst2)
 
 
-def test_quick_sort(arrays_set: Sequence[MutableSequence[Any]]) -> None:
-    for array in arrays_set:
-        array1, array2 = itertools.tee(array)
-        assert my_sort.quick_sort(list(array1)) == sorted(list(array2))
+def test_quick_sort(lists_set: Sequence[MutableSequence[Any]]) -> None:
+    for lst in lists_set:
+        lst1, lst2 = lst, copy.copy(lst)
+        assert my_sort.quick_sort(lst1) == sorted(lst2)
 
 
 if __name__ == "__main__":
